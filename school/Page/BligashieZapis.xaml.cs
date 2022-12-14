2f478cc1440172acd726a0ec766b07a29301583d
nasty <nastytrifono03@gmail.com>
2022-12-15 02:48:03 +0300

    Оставшееся время

--- school/Page/BligashieZapis.xaml.cs
+++ school/Page/BligashieZapis.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace school.Page
 {
@@ -27,6 +28,27 @@ namespace school.Page
             DateTime data = date.AddDays(2);
             List<ClientService> ser = ClassPage.Base.BD.ClientService.Where(x => x.StartTime >= DateTime.Today && x.StartTime < data).ToList();
             Zapisi.ItemsSource = ser.OrderBy(x => x.StartTime).ToList();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(30);
+            dispatcherTimer.Tick += dtTicker;
+            dispatcherTimer.Start();
+        }
+        private void loadedData()
+        {
+            List<ClientService> clientServices = ClassPage.Base.BD.ClientService.ToList();
+            clientServices = clientServices.Where(x => x.StartTime >= DateTime.Now).ToList(); // Фильтрация по дате начала
+            DateTime endDateTime = DateTime.Today.AddDays(2).AddTicks(-1); // Конец завтрашнего дня
+            clientServices = clientServices.Where(x => x.StartTime < endDateTime).ToList(); // Фильтрация по дате окончания
+            clientServices.Sort((x, y) => x.StartTime.CompareTo(y.StartTime));
+            Zapisi.ItemsSource = clientServices;
+        }
+        private void dtTicker(object sender, EventArgs e)
+        {
+            loadedData();
         }
     }
 }
